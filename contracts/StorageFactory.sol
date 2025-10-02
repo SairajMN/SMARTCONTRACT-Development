97a0243b22f675c0e19b58b21c2133007a58be61
SairajMN <sairaj.nadaf@gmail.com>
2025-10-02 23:31:09 +0530

    storage factory contract - learning abt the imports and inhertance

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
+
+//In this contract learning abt the imports and inheritance in SOLIDITY 
 
 contract storagefactory {
+    function createStorage() public {
+        
+    }
     
 }
 
